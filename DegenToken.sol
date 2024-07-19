cc2b28a7f755512ddc7ea287a15009980b4133f0
Aman72095 <amanpatel52268@gmail.com>
2024-07-20 01:05:49 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,72 +1,67 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "hardhat/console.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
-
     constructor() ERC20("Degen", "DGN") {}
 
-        function mint(address addressof, uint256 _amt) public onlyOwner{
-            _mint(addressof, _amt);
-        }
-        function transferTokens(address beneficiary, uint _amt) external{
-            require(balanceOf(msg.sender) >= _amt, "Invalid Owner !!");
-            approve(msg.sender,_amt);
-            transferFrom(msg.sender, beneficiary,_amt);
-        }
-        function balanceGetter() external view returns(uint){
-           return balanceOf(msg.sender);
-        }
-        function burnTokens(uint _amt) external{
-            require(balanceOf(msg.sender)>= _amt, "Insufficient Tokens Present");
-            _burn(msg.sender,_amt);
-        }
-        function gaminghub() public pure returns(string memory) {
-            return "1.Diamond league  NFT=100\n 2.Crown League NFT=200 \n 3.Silver League NFT=300 \n 4.Bronze League NFT=400 \n";
-        }
-        function pointsLeague(uint _points)public pure returns(string memory){
-            require(_points>=100,"You are not in any league");
-            if(_points==100){
-                return "Congratulation you are in diamond league";
-            }
-            else if(_points ==200){
-                return "Congratulation you are in crown league";
-            }
-            else if(_points ==300){
-                return "Congratulation you are in silver league";
-            }
-            else if(_points ==400){
-                return "Congratulation you are in bronze league";
-            }
+    function mint(address _account, uint256 _amount) public onlyOwner {
+        _mint(_account, _amount);
+    }
+
+    function transferTokens(address _to, uint256 _amount) external {
+        require(balanceOf(msg.sender) >= _amount, "Insufficient balance");
+        _transfer(msg.sender, _to, _amount);
+    }
+
+    function getTokenBalance() external view returns (uint256) {
+        return balanceOf(msg.sender);
+    }
+
+    function burnTokens(uint256 _amount) external {
+        require(balanceOf(msg.sender) >= _amount, "Insufficient tokens");
+        _burn(msg.sender, _amount);
+    }
+
+    function distributeTokens(uint256 _gamesWon) external {
+        require(_gamesWon >= 1 && _gamesWon <= 4, "Invalid number of games won");
+
+        uint256 tokensToDistribute = 0;
+
+        if (_gamesWon == 1) {
+            tokensToDistribute = 100;
+        } else if (_gamesWon == 2) {
+            tokensToDistribute = 250;
+        } else if (_gamesWon == 3) {
+            tokensToDistribute = 500;
+        } else if (_gamesWon == 4) {
+            tokensToDistribute = 1000;
         }
 
-        function reedemTokens(uint choice) external payable{
-            require(choice>0,"Invalid selection");
-            if(choice ==1){
-                approve(msg.sender, 100);
-                transferFrom(msg.sender, owner(), 100);
-                console.log("Congrats you have redeemed 100 tokens ");
-            }
-            else if(choice ==2){
-                approve(msg.sender, 200);
-                transferFrom(msg.sender, owner(), 200);
-                 console.log("Congrats you have redeemed 200 tokens ");
-            }
-            else if(choice ==3){
-                approve(msg.sender, 300);
-                transferFrom(msg.sender, owner(), 300);
-                 console.log("Congrats you have redeemed 300 tokens ");
-            }
-            else if(choice ==4){
-                approve(msg.sender, 400);
-                transferFrom(msg.sender, owner(), 400);
-                 console.log("Congrats you have redeemed 400 tokens ");
-            }
+        require(tokensToDistribute > 0, "Invalid number of tokens to distribute");
+        require(balanceOf(owner()) >= tokensToDistribute, "Insufficient contract balance");
+
+        _transfer(owner(), msg.sender, tokensToDistribute);
+    }
+
+    function getDistributionDetails(uint256 _gamesWon) external pure returns (string memory) {
+        require(_gamesWon >= 1 && _gamesWon <= 4, "Invalid number of games won");
+
+        string memory distributionDetails;
+
+        if (_gamesWon == 1) {
+            distributionDetails = "1 game won: 100 tokens";
+        } else if (_gamesWon == 2) {
+            distributionDetails = "2 games won: 250 tokens";
+        } else if (_gamesWon == 3) {
+            distributionDetails = "3 games won: 500 tokens";
+        } else if (_gamesWon == 4) {
+            distributionDetails = "4 games won: 1000 tokens";
         }
 
+        return distributionDetails;
+    }
 }
