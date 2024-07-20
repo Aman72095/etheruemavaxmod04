b8fb4a9e6cd1f6c269ad636a3ee4cf9516b31f00
Aman72095 <amanpatel52268@gmail.com>
2024-07-20 21:58:59 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -4,64 +4,69 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
-    constructor() ERC20("Degen", "DGN") {}
 
-    function mint(address _account, uint256 _amount) public onlyOwner {
-        _mint(_account, _amount);
+    constructor(address initialOwner) ERC20("Degen", "DGN") Ownable(initialOwner) {}
+
+    function mint(address addressof, uint256 _amt) public onlyOwner {
+        _mint(addressof, _amt);
     }
 
-    function transferTokens(address _to, uint256 _amount) external {
-        require(balanceOf(msg.sender) >= _amount, "Insufficient balance");
-        _transfer(msg.sender, _to, _amount);
+    function transferTokens(address beneficiary, uint256 _amt) external {
+        require(balanceOf(msg.sender) >= _amt, "Invalid Owner !!");
+        transfer(beneficiary, _amt);
     }
 
-    function getTokenBalance() external view returns (uint256) {
+    function balanceGetter() external view returns (uint256) {
         return balanceOf(msg.sender);
     }
 
-    function burnTokens(uint256 _amount) external {
-        require(balanceOf(msg.sender) >= _amount, "Insufficient tokens");
-        _burn(msg.sender, _amount);
+    function burnTokens(uint256 _amt) external {
+        require(balanceOf(msg.sender) >= _amt, "Insufficient Tokens Present");
+        _burn(msg.sender, _amt);
     }
 
-    function distributeTokens(uint256 _gamesWon) external {
-        require(_gamesWon >= 1 && _gamesWon <= 4, "Invalid number of games won");
-
-        uint256 tokensToDistribute = 0;
-
-        if (_gamesWon == 1) {
-            tokensToDistribute = 100;
-        } else if (_gamesWon == 2) {
-            tokensToDistribute = 250;
-        } else if (_gamesWon == 3) {
-            tokensToDistribute = 500;
-        } else if (_gamesWon == 4) {
-            tokensToDistribute = 1000;
-        }
-
-        require(tokensToDistribute > 0, "Invalid number of tokens to distribute");
-        require(balanceOf(owner()) >= tokensToDistribute, "Insufficient contract balance");
-
-        _transfer(owner(), msg.sender, tokensToDistribute);
+    function gaminghub() public pure returns (string memory) {
+        return "1. Diamond league NFT=100\n2. Crown League NFT=200\n3. Silver League NFT=300\n4. Bronze League NFT=400\n";
     }
 
-    function getDistributionDetails(uint256 _gamesWon) external pure returns (string memory) {
-        require(_gamesWon >= 1 && _gamesWon <= 4, "Invalid number of games won");
+    function pointsLeague(uint256 _points) public pure returns (string memory) {
+        require(_points >= 100, "You are not in any league");
+        if (_points == 100) {
+            return "Congratulations, you are in the Diamond League";
+        } else if (_points == 200) {
+            return "Congratulations, you are in the Crown League";
+        } else if (_points == 300) {
+            return "Congratulations, you are in the Silver League";
+        } else if (_points == 400) {
+            return "Congratulations, you are in the Bronze League";
+        } else {
+            return "You are not in any specific league";
+        }
+    }
 
-        string memory distributionDetails;
+    function redeemTokens(uint256 choice) external payable {
+        require(choice > 0, "Invalid selection");
+        uint256 amount;
 
-        if (_gamesWon == 1) {
-            distributionDetails = "1 game won: 100 tokens";
-        } else if (_gamesWon == 2) {
-            distributionDetails = "2 games won: 250 tokens";
-        } else if (_gamesWon == 3) {
-            distributionDetails = "3 games won: 500 tokens";
-        } else if (_gamesWon == 4) {
-            distributionDetails = "4 games won: 1000 tokens";
+        if (choice == 1) {
+            amount = 100;
+        } else if (choice == 2) {
+            amount = 200;
+        } else if (choice == 3) {
+            amount = 300;
+        } else if (choice == 4) {
+            amount = 400;
+        } else {
+            revert("Invalid choice");
         }
 
-        return distributionDetails;
+        require(balanceOf(msg.sender) >= amount, "Insufficient Tokens Present");
+
+        transfer(owner(), amount);
+        console.log("Congrats, you have redeemed", amount, "tokens");
     }
 }
+
